bef48d037d0e1c3e006362f83a1e997cb169ffe1
Volodymyr Protsenko <62334338+Allduiin@users.noreply.github.com>
2021-02-25 14:58:31 +0200

    Added Recipe and ChildRecipe model (#2)

---
+++ src/main/java/binary/test/task/model/Recipe.java
@@ -0,0 +1,22 @@
+package binary.test.task.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime dateCreated;
+    private String description;
+    @OneToMany
+    private List<ChildRecipe> recipes;
+}
